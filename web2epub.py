17b9745aa2c6d7d409601c229c6d94d8ecba4256
Gaurav Manek <gaurav@gauravmanek.com>
2016-03-26 19:31:18 -0400

    Added support for directory output.

--- web2epub.py
+++ web2epub.py
@@ -35,14 +35,18 @@ def run(args):
     print "Scraping Website";
     web = scrape(args.url, styler, cache=(not args.no_cache));
     
-    fout = None;
-    if args.out:
-        fout = args.out[0];
+    fout = title_fn(web[0]["title"]);
+    if args.out and len(args.out) > 0 and len(args.out[0]) > 0:
+        # If args.out is a directory, then we use our custom filename, otherwise
+        # we use the given filename.
+        if args.out[0][-1] == "/":
+            fout = args.out[0] + fout;
+        else:
+            fout = args.out[0];
+
         if not fout.lower().endswith(".epub"):
             fout = fout + ".epub";
-    else:
-        fout = title_fn(web[0]["title"]);
-
+    
     
     print "Building ePub";
     epub(web, fout, styler, args);
@@ -54,15 +58,14 @@ if __name__ == "__main__":
     # Parse incoming arguments:
     parser = argparse.ArgumentParser(description='Converts articles/posts/entries from online sources into an ePub file.');
     parser.add_argument('url', type=arg_url, help='A URL to start from.');
-    parser.add_argument('-o', '--out', type=str, metavar="OUTPUT", nargs=1, help='The filename to write to. epub automatically appended if necessary.');
+    parser.add_argument('-o', '--out', type=str, metavar="OUTPUT", nargs=1, help='The directory or filename to write to. epub automatically appended if necessary. The target directory must exist.');
     parser.add_argument('-s', '--style', type=str, metavar="STYLE", nargs=1, default=["def"], help='The output style to use.');
     parser.add_argument('-c', '--cover', type=str, metavar="COVER", nargs=1, default="png", help='The cover generator to use, if the parser does not find it.');
     parser.add_argument('--no-cover', action='store_const', default=False, const=True, help='Do not generate a cover page if one does not already exist.');
     parser.add_argument('--no-cache', action='store_const', default=False, const=True, help='Force a cache miss on this request.');
 
-    # args = parser.parse_args();
-    args = parser.parse_args(["http://m.fanfiction.net/s/5483280/1/Harry-Potter-and-the-Champion-s-Champion", "--no-cache", "-s",  "ngp"]);
-   
+    args = parser.parse_args();
+     
     try:
         plugins.load();
         run(args);
